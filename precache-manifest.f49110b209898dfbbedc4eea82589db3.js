af0fcab617079607855b93257d13b1f2ca9f4b34
Milinda Ranawaka <49589047+MilindaRanawaka@users.noreply.github.com>
2020-03-23 00:11:57 +0530

    Updates

---
+++ precache-manifest.f49110b209898dfbbedc4eea82589db3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a0addbcd206960f07ce0c8f749ae67",
+    "url": "/Covid-19-Sri-Lanka-and-Global-Tracker/index.html"
+  },
+  {
+    "revision": "043b35bda947e5dddf04",
+    "url": "/Covid-19-Sri-Lanka-and-Global-Tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3e22b2c44768248f636",
+    "url": "/Covid-19-Sri-Lanka-and-Global-Tracker/static/js/2.99017f9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid-19-Sri-Lanka-and-Global-Tracker/static/js/2.99017f9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043b35bda947e5dddf04",
+    "url": "/Covid-19-Sri-Lanka-and-Global-Tracker/static/js/main.72c93b13.chunk.js"
+  },
+  {
+    "revision": "607a75649912ec98e703",
+    "url": "/Covid-19-Sri-Lanka-and-Global-Tracker/static/js/runtime-main.662c87a8.js"
+  }
+]);
